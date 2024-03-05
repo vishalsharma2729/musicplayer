014b9c257a89b55420389207bf7779ce369c2219
vishal sharma <vishalsharma2729>
2024-03-05 15:04:07 +0530

    add songs

--- index.js
+++ index.js
@@ -9,36 +9,18 @@ let nex = document.querySelector("#next");
 });
   let music = [
     "audio/Hanuman Chalisa-(PagalWorld).mp3",
-    "music/WhatsApp Audio 2024-02-13 at 3.47.00 PM (1).mpeg",
-    "music/WhatsApp Audio 2024-02-13 at 3.46.59 PM (10).mpeg",
-    "music/128-Tere Naam - Tere Naam 128 Kbps.mp3",
-    "music/Tune Jo Na Kaha (slowed + reverbed)(KoshalWorld.Com).mp3",
-   "music/WhatsApp Audio 2024-02-13 at 3.46.59 PM.mpeg",
-  "music/WhatsApp Audio 2024-02-13 at 3.46.59 PM (1).mpeg",
-  "music/WhatsApp Audio 2024-02-13 at 3.46.59 PM (3).mpeg",
- " music/Hiriye_6.mp3",
- "music/Kyo Kisi Ko - Tere Naam 320 Kbps.mp3",
- "music/Main Vichara.mp3",
-  "music/WhatsApp Audio 2024-02-13 at 3.46.59 PM (4).mpeg",
-  "music/WhatsApp Audio 2024-02-13 at 3.46.59 PM (6).mpeg",
-  "music/WhatsApp Audio 2024-02-13 at 3.46.59 PM (9).mpeg",
-  "music/WhatsApp Audio 2024-02-13 at 3.47.00 PM (2).mpeg",
-  "music/WhatsApp Audio 2024-02-13 at 3.47.00 PM (4).mpeg",
-  "music/WhatsApp Audio 2024-02-13 at 3.47.00 PM (5).mpeg",
-  "music/WhatsApp Audio 2024-02-13 at 3.47.00 PM (6).mpeg",
-"music/WhatsApp Audio 2024-02-13 at 3.47.00 PM (8).mpeg",
-"music/WhatsApp Audio 2024-02-13 at 3.47.00 PM (9).mpeg",
-"music/WhatsApp Audio 2024-02-13 at 3.47.00 PM.mpeg"
+    "audio/Ram-Aayenge(PaglaSongs).mp3",
+    "audio/Bajrang Baan Lofi(PagalWorld.com.cm).mp3"
  ]
 
  let sum =0;
 audio.src = music[sum];
-no.value=sum;
+no.value=sum+1;
 
   const next = ()=>{
 if(sum<music.length-1){
     audio.src=music[sum+=1]
-    no.value=sum;
+    no.value=sum+1;
     audio.play();
     pre.disabled = false;
     pre.className="btn"
@@ -51,7 +33,7 @@ if(sum<music.length-1){
   const prev = ()=>{
 if(sum>0){
     audio.src=music[sum-=1]
-    no.value=sum;
+    no.value=sum+1;
     audio.play()
     nex.disabled=false;
     nex.className="btn";
